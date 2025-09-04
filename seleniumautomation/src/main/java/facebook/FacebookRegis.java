9bfec3bb4c63863fcc3878b47aa4cb5537033f49
gokilakannan54 <gokilakannan@gmail.com>
2025-09-03 21:44:00 -0400

    myhitbashcommit

--- seleniumautomation/src/main/java/facebook/FacebookRegis.java
+++ seleniumautomation/src/main/java/facebook/FacebookRegis.java
@@ -11,7 +11,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class FacebookRegis {
 	//aceessmodifier returntype methodname();
 	
-	@Test
+	
 	public void facebookRegis() throws InterruptedException {
 		
     WebDriverManager.chromedriver().setup();
